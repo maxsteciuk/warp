758896da678aeb0fea77adb6ceb0d5f48b417c8f
Maksym Stetsyuk <maxsteciuk@gmail.com>
2020-02-21 18:40:49 -0500

    1. Add cmake build capability ; 2. Add FreeBSD support ; 3. Comment out reqwest which causes unstable build for warp-packer

--- warp-packer/src/main.rs
+++ warp-packer/src/main.rs
@@ -3,7 +3,8 @@ extern crate dirs;
 extern crate flate2;
 #[macro_use]
 extern crate lazy_static;
-extern crate reqwest;
+//TODO: reqwest contains unstable libraries
+//extern crate reqwest;
 extern crate tar;
 extern crate tempdir;
 
@@ -27,16 +28,20 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 const RUNNER_MAGIC: &[u8] = b"tVQhhsFFlGGD3oWV4lEPST8I8FEPP54IM0q7daes4E1y3p2U2wlJRYmWmjPYfkhZ0PlT14Ls0j8fdDkoj33f2BlRJavLj3mWGibJsGt5uLAtrCDtvxikZ8UX2mQDCrgE\0";
 
-const RUNNER_LINUX_X64: &[u8] = include_bytes!("../../target/x86_64-unknown-linux-gnu/release/warp-runner");
-const RUNNER_MACOS_X64: &[u8] = include_bytes!("../../target/x86_64-apple-darwin/release/warp-runner");
-const RUNNER_WINDOWS_X64: &[u8] = include_bytes!("../../target/x86_64-pc-windows-gnu/release/warp-runner.exe");
+//TODO: 1. Uncomment warp-runner path for the platform where it is to be run
+//      2. Implement proper cross-platform handling
+//const RUNNER_LINUX_X64: &[u8] = include_bytes!("../../build/x86_64-unknown-linux-gnu/release/warp-runner");
+//const RUNNER_MACOS_X64: &[u8] = include_bytes!("../../build/x86_64-apple-darwin/release/warp-runner");
+//const RUNNER_WINDOWS_X64: &[u8] = include_bytes!("../../build/x86_64-pc-windows-gnu/release/warp-runner.exe");
+const RUNNER_FREEBSD_X64: &[u8] = include_bytes!("../../build/x86_64-unknown-freebsd/release/warp-runner");
 
 lazy_static! {
     static ref RUNNER_BY_ARCH: HashMap<&'static str, &'static [u8]> = {
         let mut m = HashMap::new();
-        m.insert("linux-x64", RUNNER_LINUX_X64);
-        m.insert("macos-x64", RUNNER_MACOS_X64);
-        m.insert("windows-x64", RUNNER_WINDOWS_X64);
+	m.insert("freebsd-x64", RUNNER_FREEBSD_X64);
+        //m.insert("linux-x64", RUNNER_LINUX_X64);
+        //m.insert("macos-x64", RUNNER_MACOS_X64);
+        //m.insert("windows-x64", RUNNER_WINDOWS_X64);
         m
     };
 }
@@ -197,3 +202,4 @@ fn main() -> Result<(), Box<Error>> {
     println!("All done");
     Ok(())
 }
+
